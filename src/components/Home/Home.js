15212b499a7a1964537ec055e0fe478e58c954f9
p.maniswaroop <116976951+manipinnaka1125@users.noreply.github.com>
2025-06-11 07:00:32 +0530

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
 import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
 
 function Home() {
- 
   return (
     <section>
       <Container fluid className="home-section" id="home">
@@ -27,7 +25,10 @@ function Home() {
                 <strong className="main-name"> Mani Swaroop</strong>
               </h1>
 
-             
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
 
             <Col md={5} style={{ paddingBottom: 20 }}>
               <img
